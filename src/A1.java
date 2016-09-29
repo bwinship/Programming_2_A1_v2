8c3d1647bcdbba00b43fc69007d05f150c0e356e
jc314116 <brandon.winship@my.jcu.edu.au>
2016-09-29 11:21:54 +1000

    I have put my user stories in a spereate file, just so I can edit it while working on my project instead of constantly re-downloading to edit. I have done a welcome message method, a menu options method and a menu choice function.

--- src/A1.java
+++ src/A1.java
@@ -1,43 +1,67 @@
-/**
- * Created by jc314116 on 22/09/16.
- */
+//* Programming_2 Assignment: By Brandon Winship, 13144404
 import javax.swing.*;
+import java.util.Scanner;
 
-public class A1 {
+class A1 {
     public static void main(String[] args) {
-
+        int user_choice = 0;
+        Welcome();
+        Menu();
+        Menu_Choice(user_choice);
     }
 
-    public static void Welcome(){
+    private static void Welcome(){
         System.out.println("Hey, welcome to the Mineral Supertrumps Game!!!");
     }
 
     //* A player can exit game or start a new game
-    public static void Menu (){
+    private static void Menu (){
         System.out.println("1. Play Game");
         System.out.println("2. Exit Game");
+    }
 
+    private static int Menu_Choice(int user_choice){
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Please make your choice: ");
+        int user_input = scan.nextInt();
+
+        int pass = 1;
+        while (pass ==1){
+            if (user_input == 1){
+                user_choice = user_input;
+                pass = 0;
+            }
+            if (user_input == 2){
+                user_choice = user_input;
+                pass = 0;
+            }
+            else{
+                System.out.print("Invalid choice! Please try again: ");
+                user_input = scan.nextInt();
+            }
+        }
+        return user_choice;
     }
 
     //* If player is randomly selected as dealer, that player can shuffle cards
-    public static void is_dealer(){
+    //public static void is_dealer(){
 
-    }
+    //}
 
 
     //* Player can look at their cards but cannot show other players their cards
-    public static void view_player_cards(){
+    // public static void view_player_cards(){
 
-    }
+    //}
 
     //* If a player is sitting left of the dealer, player goes first
-    public static void who_goes_first(){
+    // public static void who_goes_first(){
 
-    }
+    //}
 
     //* Player can place mineral card on table, declare mineral name, one of the five playing categories and the top
     // value of that category
-    public static
+    // public static
 
     //* If it’s the player’s second turn, player can play a card but card must have a higher value in the playing
     // category than the last player
