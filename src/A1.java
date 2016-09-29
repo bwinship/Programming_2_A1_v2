de2a0db439620b06d5dd55cf4f1cfd71a9fe3cc7
Brandon Winship <jc314116@ad.jcu.edu.au>
2016-09-29 10:08:29 +1000

    Had to make a new repository, it kept not saving my files, unsure if it was my fault or not. Either way, markers can view my previous commits and progress on my old repository here: https://github.com/bwinship/Programming_2_A1

---
+++ src/A1.java
@@ -0,0 +1,59 @@
+/**
+ * Created by jc314116 on 22/09/16.
+ */
+import javax.swing.*;
+
+public class A1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void Welcome(){
+        System.out.println("Hey, welcome to the Mineral Supertrumps Game!!!");
+    }
+
+    //* A player can exit game or start a new game
+    public static void Menu (){
+        System.out.println("1. Play Game");
+        System.out.println("2. Exit Game");
+
+    }
+
+    //* If player is randomly selected as dealer, that player can shuffle cards
+    public static void is_dealer(){
+
+    }
+
+
+    //* Player can look at their cards but cannot show other players their cards
+    public static void view_player_cards(){
+
+    }
+
+    //* If a player is sitting left of the dealer, player goes first
+    public static void who_goes_first(){
+
+    }
+
+    //* Player can place mineral card on table, declare mineral name, one of the five playing categories and the top
+    // value of that category
+    public static
+
+    //* If it’s the player’s second turn, player can play a card but card must have a higher value in the playing
+    // category than the last player
+
+
+    //* Player can play a trump card, then they can follow the instructions on their card
+
+
+    //* If the player does not have a higher card to play, player can pass and draw a card from the deck
+
+
+    //* A player is allowed to pass even if they have a card that can be played
+
+
+    //* A player can win by being the first person to lose all their cards
+
+
+
+}
